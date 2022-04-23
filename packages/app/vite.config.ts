45d0489ca59ec0fc653c7e27d20bf54db570a973
naybnet <6021316+Naybnet@users.noreply.github.com>
2022-04-23 23:24:24 +0200

    feat(global): add app dependencies for ui, router, store and translations

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,15 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { fileURLToPath, URL } from 'url'
 
-// const path = require("path")
+import Components from 'unplugin-vue-components/vite'
+import { HeadlessUiResolver } from 'unplugin-vue-components/resolvers'
+import Layouts from 'vite-plugin-vue-layouts'
+import AutoImport from 'unplugin-auto-import/vite'
+import Icons from 'unplugin-icons/vite'
+import VueI18n from '@intlify/vite-plugin-vue-i18n'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({ resolvers: [HeadlessUiResolver()], dts: 'src/components.d.ts' }),
+    Layouts(),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'vue-i18n'],
+      dts: 'src/auto-imports.d.ts',
+    }),
+    Icons({ autoInstall: true }),
+    VueI18n({
+      include: [fileURLToPath(new URL('locales/**', import.meta.url))],
+    }),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
